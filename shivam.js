6ed3ea2809b43f87ad03547595b66443327c9c6a
MrShivam7 <122256610+MrShivam7@users.noreply.github.com>
2024-01-08 22:04:13 +0530

    Update shivam.js

--- shivam.js
+++ shivam.js
@@ -13,7 +13,7 @@ var i = 0;
             "The book was closed < But... < The story was incomplete",
             "Pal pal tadpe Jis pal ke liye < Vo pal bhi aya Kuch pal ke liye < Socha is pal ko rok lu har pal ke liye < Pr vo pal bhi thehra kuch pal ke liye",
             "Aur tum jo sab mujhe kehte rehte ho ki usse bhul jao < Aur tum jo sab mujhe kehte rehte ho ki usse bhul jao < Tum hi btao kisiko bhulana itna asan hota hai kya < jis ko dil me basaya ho fhir, dil se nikalna itna asan hota hai kya < asan hai kya bhulana us shaks se ki hui baate < asan hai kya bhulana uske saath bitai hui vo raate < itna asan hota hai kya kisi ko paraya kr dena jis me tumhari duniya basti ho < usse apni duniya se alwida kr dena < are vo dil kashi nhi, ishq hai mera < are vo dil kashi nhi, ishq hai mera < mai chaha kr bhi usse bhula nhi paata < uske liye tadapte huye bhi us se baat nhi krta < ab dekho yaaro is se zyada sabar mai dikha nhi pata < vo meri hai jinn khawabo me, un khawabo ko mai mita nhi pata < mai har roz sochta hu, ki ab usse yaad na kru < par ab uski yaad ke bina mujhe saans nhi aata!! ",
-            "",
+            "May be... < We should ended it before it < Got started",
             "",
             "",
             "",
